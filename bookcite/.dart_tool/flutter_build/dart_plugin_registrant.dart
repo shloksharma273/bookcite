326e7be09a4a9054e1fc671ee745602ef4f73608
shloksharma273 <shloksharma273@gmail.com>
2025-07-23 21:02:40 +0530

    added upload page

--- bookcite/.dart_tool/flutter_build/dart_plugin_registrant.dart
+++ bookcite/.dart_tool/flutter_build/dart_plugin_registrant.dart
@@ -6,10 +6,15 @@
 // @dart = 3.6
 
 import 'dart:io'; // flutter_ignore: dart_io_import.
+import 'package:file_picker/file_picker.dart';
 import 'package:path_provider_android/path_provider_android.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:path_provider_foundation/path_provider_foundation.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:path_provider_linux/path_provider_linux.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:path_provider_foundation/path_provider_foundation.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:path_provider_windows/path_provider_windows.dart';
 
 @pragma('vm:entry-point')
@@ -18,6 +23,15 @@ class _PluginRegistrant {
   @pragma('vm:entry-point')
   static void register() {
     if (Platform.isAndroid) {
+      try {
+        FilePickerIO.registerWith();
+      } catch (err) {
+        print(
+          '`file_picker` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
       try {
         PathProviderAndroid.registerWith();
       } catch (err) {
@@ -29,6 +43,15 @@ class _PluginRegistrant {
 
     } else if (Platform.isIOS) {
       try {
+        FilePickerIO.registerWith();
+      } catch (err) {
+        print(
+          '`file_picker` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
         PathProviderFoundation.registerWith();
       } catch (err) {
         print(
@@ -38,6 +61,15 @@ class _PluginRegistrant {
       }
 
     } else if (Platform.isLinux) {
+      try {
+        FilePickerLinux.registerWith();
+      } catch (err) {
+        print(
+          '`file_picker` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
       try {
         PathProviderLinux.registerWith();
       } catch (err) {
@@ -49,6 +81,15 @@ class _PluginRegistrant {
 
     } else if (Platform.isMacOS) {
       try {
+        FilePickerMacOS.registerWith();
+      } catch (err) {
+        print(
+          '`file_picker` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
         PathProviderFoundation.registerWith();
       } catch (err) {
         print(
@@ -58,6 +99,15 @@ class _PluginRegistrant {
       }
 
     } else if (Platform.isWindows) {
+      try {
+        FilePickerWindows.registerWith();
+      } catch (err) {
+        print(
+          '`file_picker` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
       try {
         PathProviderWindows.registerWith();
       } catch (err) {
